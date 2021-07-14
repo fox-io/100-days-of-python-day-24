98578fb7249f54e6a0380569fbc4879e9484a8f3
fox-io <gitlab.fox-io@foxdata.io>
2021-07-13 20:27:56 -0500

    Add main.py stub.

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Day 24 of 100 Days of Python course on Udemy.
+
+https://www.udemy.com/course/100-days-of-code/
+
+(c)2021 John Mann <gitlab.fox-io@foxdata.io>
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
