44f5bfcc0c2937bc085c17b34abe5edde3bea5a9
fox-io <gitlab.fox-io@foxdata.io>
2021-07-13 20:38:55 -0500

    Add project information.

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ Day 24 of 100 Days of Python course on Udemy.
 
 https://www.udemy.com/course/100-days-of-code/
 
+Topic: Files, Directories, and Paths
+Project: Mail Merge
+Description: Given a list of names and a template .txt file, output a file named with each name containing the
+template .txt filled in with that name.
+
 (c)2021 John Mann <gitlab.fox-io@foxdata.io>
 """
 
